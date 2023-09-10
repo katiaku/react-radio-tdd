da92b7b13dd20a514bfce1fa1bf91e5d1d6ee182
Katia <kushnir.ekaterina@gmail.com>
2023-09-10 14:34:41 +0200

    Define testing steps

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,16 @@
+// Radio Streaming Station Search and Selection Application
 
+// 0 - The application must render correctly
+
+// 1 - The name of the application should be displayed somewhere => "OpenRadioCamp"
+
+// 2 - We should be able to search for radio stations by name
+// 2a - The application must have an input field with the placeholder => "Enter the radio station's name"
+// 2b - The application must have a search button => Text "Search"
+// 2c - When we click on the search button, the search function should be executed only once
+
+// 3 - List of radio stations
+// 3a - There must be a list of radio stations
+// 3b - The list must initialize as empty
+// 3c - When a valid search is performed, the list should display at least one result
+// 3d - When an invalid search is performed (no match found), the list should display a message "No radio stations found for this search"
