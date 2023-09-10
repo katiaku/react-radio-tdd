c6910f3dae9d0bf3f83e1fdc8ac126b8283dacb7
Katia <kushnir.ekaterina@gmail.com>
2023-09-10 17:58:59 +0200

    Test 3c - When a valid search is performed, the list should display at least one result

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const[search, setSearch] = useState("");
+  const[list, setList] = useState([]);
+
+  const doSearch = () => {
+    const url = `https://fr1.api.radio-browser.info/json/stations/byname/${search}`;
+    axios.get(url)
+      .then(r => setList(r.data))
+      .catch(e => console.error(e))
+  }
+
   return (
     <div className="App">
       <h1>Welcome to <span>OpenRadioCamp</span> App</h1>
-      <input type='text' placeholder="Enter the radio station's name" />
-      <button>Search</button>
+      <input
+        type='text'
+        placeholder="Enter the radio station's name"
+        value={ search }
+        onChange={e => setSearch(e.target.value)}
+      />
+      <button onClick={ doSearch }>Search</button>
+      {list.length > 0 && <div aria-label='lenght-not-null'></div>}
       <section aria-label="list-stations">
-        
+        {list.map((station, i) => <div key={i}>{station.name}</div>)}
       </section>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import { fireEvent, render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 import App from "./App"
 
+const mockArray = [{ "changeuuid": "cd84ce7d-8c80-4db2-9605-a2dcea1bc224", "stationuuid": "a4a3c3c4-d499-48e0-bec1-226868d22968", "serveruuid": null, "name": "__COUNTRYHITS.FM__ by rautemusik (rm.fm)", "url": "https://country-high.rautemusik.fm/?ref=radiobrowser", "url_resolved": "https://rautemusik-de-hz-fal-stream12.radiohost.de/country?ref=radiobrowser&listenerrmid=31363339393539303830-323030313a313966303a363830313a313964373a353430303a3266663a666537313a62646237-3630323938-53747265616d436865636b426f742f302e312e30", "homepage": "https://countryhits.fm/", "favicon": "https://i.ibb.co/C1SspdX/country.jpg", "tags": "alternative country,alternative rock,americana,classic country,classic rock,country,hits,nashville,new country,rock,texas country", "country": "Germany", "countrycode": "DE", "iso_3166_2": null, "state": "North Rhine-Westphalia", "language": "english,german", "languagecodes": "de,en", "votes": 75, "lastchangetime": "2021-11-09 23:05:40", "lastchangetime_iso8601": "2021-11-09T23:05:40Z", "codec": "MP3", "bitrate": 192, "hls": 0, "lastcheckok": 1, "lastchecktime": "2021-12-20 00:11:22", "lastchecktime_iso8601": "2021-12-20T00:11:22Z", "lastcheckoktime": "2021-12-20 00:11:22", "lastcheckoktime_iso8601": "2021-12-20T00:11:22Z", "lastlocalchecktime": "2021-12-20 00:11:22", "lastlocalchecktime_iso8601": "2021-12-20T00:11:22Z", "clicktimestamp": "2021-12-20 04:53:01", "clicktimestamp_iso8601": "2021-12-20T04:53:01Z", "clickcount": 119, "clicktrend": -3, "ssl_error": 0, "geo_lat": null, "geo_long": null, "has_extended_info": false }];
+
 beforeEach(() => render(<App />));
 
 // 0 - The application must render correctly
@@ -60,5 +62,16 @@ describe('3 - List of radio stations', () => {
         const list = screen.getByLabelText('list-stations');
         const childrenCount = list.childElementCount;
         expect(childrenCount).toBe(0);
+    })
+    test('3c - When a valid search is performed, the list should display at least one result', () => {
+        const placeholdertext = "Enter the radio station's name";
+        const input = screen.getByPlaceholderText(placeholdertext);
+        const buttontext = "Search";
+        const button = screen.getByText(buttontext);
+        fireEvent.change(input, { target: { value: 'Country' }});
+        fireEvent.click(button);
+        const list = screen.getByLabelText('list-stations');
+        const childrenCount = list.childElementCount;
+        expect(childrenCount).toBeGreaterThanOrEqual(0);
     })
 })
